44b9f68deeae0c16d3ab1cf3d68623c97fd27df4
李凤焕 <huanent@icloud.com>
2020-03-16 19:35:32 +0800

    add langs load

--- PreviewServer/Program.cs
+++ PreviewServer/Program.cs
@@ -2,12 +2,12 @@
 using Kooboo.IndexedDB;
 using Kooboo.Lib.Reflection;
 using Kooboo.Mail;
-using Kooboo.Render.Controller;
 using Kooboo.Web;
 using System;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using VirtualFile;
 
 namespace PreviewServer
 {
@@ -15,7 +15,7 @@ namespace PreviewServer
     {
         static void Main(string[] args)
         {
-            AddModule();
+            AddModule("../");
 
             Kooboo.Lib.Compatible.CompatibleManager.Instance.Framework.RegisterEncoding();
             GlobalSettings.RootPath = Kooboo.Data.AppSettings.DatabasePath;
@@ -38,17 +38,51 @@ namespace PreviewServer
             Kooboo.Lib.Compatible.CompatibleManager.Instance.Framework.ConsoleWait();
         }
 
-        private static void AddModule()
+        private static void AddModule(string path)
         {
-            var slnDir= Path.GetFullPath("../../../../");
-            ModuleFile.ModuleRoots.Add(slnDir);
-            var dirs = Directory.GetDirectories(slnDir);
-            foreach (var item in dirs.Where(s=>s.ToLower().EndsWith("module")))
-            {
-                var dllPath = Path.Combine(item, "bin", "Debug", "netstandard2.0");
+            path = Path.GetFullPath(path);
+            var parent = Directory.GetParent(path).FullName;
+            var configJsons = Directory.GetFiles(parent, "*config.json", SearchOption.AllDirectories);
+            Kooboo.Render.Controller.ModuleFile.ModuleRoots.Add(parent);
 
-                if (Directory.Exists(dllPath))
+            if (configJsons.Count() > 0)
+            {
+                foreach (var item in configJsons)
                 {
+                    var moduleDic = Path.GetDirectoryName(item);
+                    LoadConfig(item);
+                    LoadFiles(moduleDic);
+                    LoadDir(moduleDic);
+                }
+            }
+
+            if (Directory.GetFiles(parent, "*.sln").Count() > 0)
+            {
+                return;
+            }
+            else
+            {
+                AddModule(parent);
+            }
+        }
+
+        private static void LoadConfig(string item)
+        {
+            var moduleName = Path.GetFileName(Path.GetDirectoryName(item));
+            var path = Path.Combine(AppSettings.ModulePath, moduleName, "config.json");
+            path = Helper.NormalizePath(path);
+            VirtualResources.Entries.TryAdd(path, new ModuleFile(item, "module"));
+        }
+
+        private static void LoadDir(string moduleDic)
+        {
+            foreach (var item in Directory.GetDirectories(moduleDic))
+            {
+                var dirName = Path.GetFileName(item);
+                if (dirName == "obj") continue;
+                if (dirName == "bin")
+                {
+                    var dllPath = Path.Combine(item, "Debug", "netstandard2.0");
                     var alldlls = Directory.GetFiles(dllPath, "*.dll", SearchOption.TopDirectoryOnly);
                     foreach (var name in alldlls)
                     {
@@ -57,6 +91,25 @@ namespace PreviewServer
                         AssemblyLoader.AddAssembly(otherAssembly);
                     }
                 }
+                else
+                {
+                    var files = Directory.GetFiles(item, "*", SearchOption.AllDirectories);
+                    foreach (var file in files)
+                    {
+                        var normalPath = Helper.NormalizePath(file);
+                        VirtualResources.Entries.TryAdd(normalPath, new ModuleFile(normalPath, "module"));
+                    }
+                }
+            }
+        }
+
+        private static void LoadFiles(string parent)
+        {
+            var files = Directory.GetFiles(parent);
+            foreach (var file in files)
+            {
+                var normalPath = Helper.NormalizePath(file);
+                VirtualResources.Entries.TryAdd(normalPath, new ModuleFile(normalPath, "module"));
             }
         }
     }
