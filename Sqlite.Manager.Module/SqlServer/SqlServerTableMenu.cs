f3d7ca17873bfd7a4383f28ab93e326fc1f32113
maolin-kooboo <maolin@kooboo.cn>
2020-03-18 16:10:38 +0800

    implement sqlserver

---
+++ Sqlite.Manager.Module/SqlServer/SqlServerTableMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using Kooboo.Data.Context;
+using Kooboo.Data.Language;
+using Kooboo.Web.Menus;
+
+namespace Sqlite.Menager.Module.SqlServer
+{
+    public class SqlServerTableMenu : ISideBarMenu
+    {
+        public SideBarSection Parent => SideBarSection.Database;
+
+        public string Name => "sqlserver.table";
+
+        public string Icon => "";
+
+        public string Url => "sqlserver.manager.module/sqlserver.html";
+
+        public int Order => 0;
+
+        public List<ICmsMenu> SubItems { get; set; }
+
+        public string GetDisplayName(RenderContext Context)
+        {
+            return Hardcoded.GetValue(Name, Context);
+        }
+    }
+}
