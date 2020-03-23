f94980347a0b05d1fe28f320cc5a8386f02b6715
李凤焕 <huanent@icloud.com>
2020-03-23 17:11:46 +0800

    improved generateZip

--- tools/moduleZip.bat
+++ tools/moduleZip.bat
@@ -3,13 +3,14 @@
 set projectDir=%1
 set projectName=%2
 set slnDir=%3
-set releaseDir=%projectDir%\bin\Release\netstandard2.0\
+set releaseDir=%projectDir%\bin\Release\netcoreapp2.1\publish\
 
 set zipFile=%releaseDir%\%projectName%.zip
 ::delete existed zipFile
 if exist "%zipFile%" (del %zipFile%)
 
-C:\Windows\System32\robocopy  %projectDir% %releaseDir%modules /s /xd kbtest .vscode bin obj Properties /xf *.cs *.csproj *.csproj.user
+C:\Windows\System32\robocopy  %slnDir%\%projectName% %releaseDir%modules /s /xd kbtest .vscode bin obj Properties /xf *.cs *.csproj *.csproj.user
+if exist %releaseDir%\PreviewServer.dll (del %releaseDir%\PreviewServer.dll)
 for %%i in (%slnDir%Kooboo\*.dll) do (
 	if exist %releaseDir%\%%~nxi (del %releaseDir%\%%~nxi)
 )
