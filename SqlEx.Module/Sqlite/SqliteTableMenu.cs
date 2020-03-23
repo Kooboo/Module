5fabbefe398d4fc0c9f592ba39dcad559ce5d35e
李凤焕 <huanent@icloud.com>
2020-03-23 15:46:12 +0800

    translate

--- SqlEx.Module/Sqlite/SqliteTableMenu.cs
+++ SqlEx.Module/Sqlite/SqliteTableMenu.cs
@@ -9,7 +9,7 @@ namespace SqlEx.Module.Sqlite
     {
         public SideBarSection Parent => SideBarSection.Database;
 
-        public string Name => "sqlite.table";
+        public string Name => Hardcoded.GetValue("sqlite.table");
 
         public string Icon => "";
 
