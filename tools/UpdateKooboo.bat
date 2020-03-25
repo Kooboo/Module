8b6d3c0bb4ec16122c692e019c6eebf82a1edadb
李凤焕 <huanent@icloud.com>
2020-03-25 18:33:43 +0800

    update kooboo lib

---
+++ tools/UpdateKooboo.bat
@@ -0,0 +1,4 @@
+rd /s /q %TMP%\_koobooTemp
+git clone https://github.com/Kooboo/Module.git %TMP%\_koobooTemp
+rd /s /q ..\Kooboo
+C:\Windows\System32\robocopy  %TMP%\_koobooTemp\template\Kooboo ..\Kooboo /s
